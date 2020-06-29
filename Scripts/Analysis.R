e88a205b5bebae6070a2610400fa24a20cbc1353
Nils Kappelmann <n.kappelmann@gmail.com>
2020-06-29 18:18:07 +0200

    worked on scripts and manuscript file

--- Scripts/Analysis.R
+++ Scripts/Analysis.R
@@ -8,32 +8,82 @@
 ## Load packages
 library("tidyverse")
 library("RColorBrewer")
-library("HDclassif")
-library("glmnet")
 library("caret")
+library("glmnet")
+library("PubHelper")
 
 
 ## Load data
+load("")
 
-
-# 1 Model Training---------------------------------------
-
-
-trainControl(method = "LOOCV")
-trainControl(method = "boot")
+## Load cytokine reference
+load("")
 
 
 
-# 2 Analysis---------------------------------------------
+# 2 Descriptive statistics-------------------------------
 
-# 2.1 High-Dimensional Discriminant Analysis-------------
+# 2.1 Baseline Tables------------------------------------
+
+## Baseline Table of clinical and sociodemographic characteristics
+baselineTable(dat, round_dec = 2, placeholder = "   ",
+              vars = c("sex_cat", "age", "socialclass_cat", 
+                       "countryorigin", "ethnicity", "employed",
+                       "t0_bdi", "t0_madrs", "t0_bsi_depr",
+                       "ward_type",
+                       "t0_cidi_diagnsum"),
+              labels = c("Sex", "Age", "Social class",
+                         "Country of Origin", "Ethnicity", "Employment status",
+                         "BDI", "MADRS", "BSI: Depression",
+                         "Hospitalisation in",
+                         "Number of psychiatric diagnoses"))
+
+## Baseline Tables of inflammatory parameters
+baselineTable(dat, round_dec = 2, placeholder = "   ", grouping.var = "crp_greater3",
+              vars = cyto_ref$vars,
+              labels = cyto_ref$labels)
 
 
-## Run hdda
-hdda.fit = hdda(data = dat, model = "All", d_select = "CV", LOO = TRUE, cv.vfold = nrow(dat),
-                scaling = FALSE, graph = TRUE)
+# 3 Model Training---------------------------------------
+
+# 3.1 Elastic Net Regression-----------------------------
 
 
-## Plot results
-plot(hdda.fit, method = "CV")
+#trainControl(method = "LOOCV")
+#trainControl(method = "boot")
+
+# Zou and Hastie’s(2005) recommended default alpha = 0.5.
+
+train(x = dat[,-y], y = dat[,y], 
+      method = "glmnet", metric = "RMSE",
+      trControl = trainControl(method = "LOOCV"))
+
+
+# 3.2 Random Forest--------------------------------------
+
+# Surpassing Variable importance threshold?
+
+
+
+
+
+# 4 Content validity analysis-----------------------------
+
+
+# 4.1 MADRS----------------------------------------------
+
+
+
+# 4.2 BSI-Depression-------------------------------------
+
+
+# 4.3 WHODAS---------------------------------------------
+
+
+# 4.4 CIDI Depression------------------------------------
+
+
+# 4.5 Dropout--------------------------------------------
+
+
 
--- Scripts/Preparation.R
+++ Scripts/Preparation.R
@@ -3,6 +3,121 @@
 # -------------------------------------------------------
 
 
-# 1 Setup------------------------------------------------
+# 1 Preparation------------------------------------------
+
+# 1.1 Setup----------------------------------------------
+
+## Load Packages
+library("tidyverse")
+
+## Load psychometric data
+load("/home/nkappelmann/OPTIMA/OPTIMA_Analyses/Data/Processed/02_PsychometricData.RData")
+
+## Load cytokine data
+cyto_dat = readRDS("/binder/mgp/datasets/2020_ImmuneDepression/cytokine/01_data_for_analysis.rds")
+
+# Save Study IDs
+cyto_dat$ID = row.names(cyto_dat)
+
+# Save variable names in cytokine reference
+cyto_ref = data.frame(vars = NA,
+                      labels = colnames(cyto_dat)[8:ncol(cyto_dat)])
+
+# Change column names by excluding hyphens and slashes
+colnames(cyto_dat) = gsub("/", "_", colnames(cyto_dat), fixed = TRUE)
+colnames(cyto_dat) = gsub("-", ".", colnames(cyto_dat), fixed = TRUE)
+colnames(cyto_dat) = gsub(" ", "_", colnames(cyto_dat), fixed = TRUE)
+cyto_ref$vars = colnames(cyto_dat)[8:ncol(cyto_dat)]
 
 
+## Merge psychometric and cytokine data
+dat = merge(dat, cyto_dat, by = "ID", all.x = TRUE)
+
+
+# 1.2 Coding---------------------------------------------
+
+
+## CRP cut-off >3mg/L
+dat$CRP_greater3 = factor(ifelse(dat$hsCRP > 3, ">3mg/L", "<=3mg/L"),
+                          levels = c("<=3mg/L", ">3mg/L"))
+
+
+## Sex as binary number and category
+dat$sex_num = ifelse(dat$sex2 == "weiblich", 1, 0)
+dat$sex_cat = ifelse(dat$sex2 == "weiblich", "Female", "Male")
+
+
+## Ward
+dat$ward_type = ifelse(grepl("ST", dat$ward, fixed = TRUE), "Ward", "Day-clinic")
+
+## Other baseline variables
+
+# Country of origin
+dat$countryorigin = ifelse(dat$countryorigin == 0, "Germany", "Other")
+
+# Ethnicity
+dat$ethnicity = ifelse(dat$ethnicity == 0, "German", "Other")
+
+# Current employment (code retired as extra category)
+dat$employed = ifelse(dat$employed == 1, "Employed", "Unemployed")
+dat[!is.na(dat$unemploymentreason) & dat$unemploymentreason == "retired", "employed"] = "Retired" 
+
+
+
+# 2 Preprocessing----------------------------------------
+
+# 2.1 LOCF of depressive symptom outcome-----------------
+
+# 2.1.1 BDI----------------------------------------------
+
+## Create empty bdi_locf variable
+dat$t7_bdi_locf = NA
+
+## Run loop to carry forward the last observed bdi-value
+for(i in 1:nrow(dat))   {
+      for(j in 7:1)   {
+            if(is.na(dat[i, "t7_bdi_locf"]) & !is.na(dat[i, paste0("t", j, "_bdi")]))     {
+                  dat[i, "t7_bdi_locf"] = dat[i, paste0("t", j, "_bdi")]
+                  }
+      }
+}
+
+
+# 2.1.2 MADRS--------------------------------------------
+
+## Create empty bdi_locf variable
+dat$t7_madrs_locf = NA
+
+## Run loop to carry forward the last observed bdi-value
+for(i in 1:nrow(dat))   {
+      for(j in c(7, 4, 0))   {
+            if(is.na(dat[i, "t7_madrs_locf"]) & !is.na(dat[i, paste0("t", j, "_madrs")]))     {
+                  dat[i, "t7_madrs_locf"] = dat[i, paste0("t", j, "_madrs")]
+            }
+      }
+}
+
+
+# 3 Age, Sex & Non-normality Corrections-----------------
+
+# 3.1 Log-transformation---------------------------------
+
+# 3.2 Age & Sex correction-------------------------------
+
+# 3.3 Scaling--------------------------------------------
+
+
+# 4 Missing Data Imputation------------------------------
+
+## Applying Nearest-Neighbour Imputation of Inflammatory Cytokines
+
+
+
+
+# 5 Save Data--------------------------------------------
+
+## Save main data
+save(dat, file = "")
+
+## Save info of inflammatory variables
+save(cyto_ref, file = "")
