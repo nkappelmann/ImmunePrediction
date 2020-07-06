f48b5e1ecb9162427bb25326365ac19488d86d0e
Nils Kappelmann <n.kappelmann@gmail.com>
2020-07-06 08:07:03 +0200

    wroked on analysis scripts and manuscript

--- Scripts/Analysis.R
+++ Scripts/Analysis.R
@@ -7,6 +7,7 @@
 
 ## Load packages
 library("tidyverse")
+library("ggcorrplot")
 library("RColorBrewer")
 library("caret")
 library("glmnet")
@@ -14,10 +15,26 @@ library("PubHelper")
 
 
 ## Load data
-load("")
+load("/binder/mgp/datasets/2020_ImmuneDepression/cytokine/Nils_Preprocessed/OPTIMA_Cytokine_PreprocessedData.RData")
 
 ## Load cytokine reference
-load("")
+load("/binder/mgp/datasets/2020_ImmuneDepression/cytokine/Nils_Preprocessed/Cytokine_Reference.RData")
+
+
+
+# 1.1 Define Parameters----------------------------------
+
+## Save predictors, covariates, and outcome variables
+x = cyto_ref[cyto_ref$no.na == 1, "vars"]
+y = "bdi_locf_improve"
+covariates = c("age_std", "sex_std")
+
+## Define trainControl
+fitControl = trainControl(method = "repeatedcv",
+                          number = 10,
+                          repeats = 10)
+
+
 
 
 
@@ -26,37 +43,132 @@ load("")
 # 2.1 Baseline Tables------------------------------------
 
 ## Baseline Table of clinical and sociodemographic characteristics
-baselineTable(dat, round_dec = 2, placeholder = "   ",
-              vars = c("sex_cat", "age", "socialclass_cat", 
-                       "countryorigin", "ethnicity", "employed",
-                       "t0_bdi", "t0_madrs", "t0_bsi_depr",
-                       "ward_type",
-                       "t0_cidi_diagnsum"),
-              labels = c("Sex", "Age", "Social class",
-                         "Country of Origin", "Ethnicity", "Employment status",
-                         "BDI", "MADRS", "BSI: Depression",
-                         "Hospitalisation in",
-                         "Number of psychiatric diagnoses"))
+Table1 = baselineTable(dat, round_dec = 2, placeholder = "   ", grouping.var = "CRP_inflamed",
+                       vars = c("sex", "age", "socialclass_cat", 
+                                "countryorigin", "ethnicity", "employed",
+                                "t0_bdi", "t0_madrs", "t0_bsi_depr",
+                                "ward_type",
+                                "t0_cidi_diagnsum"),
+                       labels = c("Sex", "Age", "Social class",
+                                  "Country of Origin", "Ethnicity", "Employment status",
+                                  "BDI", "MADRS", "BSI: Depression",
+                                  "Hospitalisation in",
+                                  "Number of psychiatric diagnoses"))
 
 ## Baseline Tables of inflammatory parameters
-baselineTable(dat, round_dec = 2, placeholder = "   ", grouping.var = "crp_greater3",
+baselineTable(dat, round_dec = 2, placeholder = "   ", grouping.var = "CRP_inflamed",
               vars = cyto_ref$vars,
               labels = cyto_ref$labels)
+
+exportPubHelpercsv(Table1, file = "./Results/Table1.csv")
 
 
 # 3 Model Training---------------------------------------
 
+## Create empty data.frame to extract fit statistics
+fit.stats = data.frame(method = character(),
+                       model = character(),
+                       r2 = numeric(),
+                       rmse = numeric(),
+                       stringsAsFactors = FALSE)
+
+
 # 3.1 Elastic Net Regression-----------------------------
 
 
-#trainControl(method = "LOOCV")
 #trainControl(method = "boot")
 
 # Zou and Hastie’s(2005) recommended default alpha = 0.5.
 
-train(x = dat[,-y], y = dat[,y], 
-      method = "glmnet", metric = "RMSE",
-      trControl = trainControl(method = "LOOCV"))
+
+
+# 3.1.1 NULL Model---------------------------------------
+
+set.seed(1)
+model.null.enet = train(x = dat[,c(x, covariates)], y = sample(dat[,y], replace = FALSE), 
+                        method = "glmnet", metric = "RMSE", #alpha = 0.5,
+                        trControl = fitControl)
+print(model.null.enet)
+
+## Save fit statistics
+fit.stats[nrow(fit.stats) + 1, ] = NA
+fit.stats[nrow(fit.stats), c("method", "model")] = c("Elastic Net", "Null")
+fit.stats[nrow(fit.stats), c("rmse", "r2")] = model.null.enet$results %>%
+      filter(RMSE == min(RMSE)) %>% 
+      select(RMSE, Rsquared)
+
+
+
+
+# 3.1.2 Baseline Model-----------------------------------
+
+set.seed(1)
+model.base.enet = train(x = dat[,covariates], y = dat[,y], 
+                         method = "glmnet", metric = "RMSE", #alpha = 0.5,
+                         trControl = fitControl)
+print(model.base.enet)
+
+## Save fit statistics
+fit.stats[nrow(fit.stats) + 1, ] = NA
+fit.stats[nrow(fit.stats), c("method", "model")] = c("Elastic Net", "Baseline")
+fit.stats[nrow(fit.stats), c("rmse", "r2")] = model.base.enet$results %>%
+      filter(RMSE == min(RMSE)) %>% 
+      select(RMSE, Rsquared)
+
+## Save LOOCF predictions
+tuningvalues = as.numeric(model.base.enet$finalModel$tuneValue)
+dat$pred.base.enet = model.base.enet$pred %>% 
+      filter(alpha == tuningvalues[1] & lambda == tuningvalues[2]) %>% 
+      arrange(rowIndex) %>% pull(pred)
+
+
+
+# 3.1.3 Cytokine Model-----------------------------------
+
+
+set.seed(1)
+model.unadj.enet = train(x = dat[,x], y = dat[,y], 
+                         method = "glmnet", metric = "RMSE", #alpha = 0.5,
+                         trControl = fitControl)
+print(model.unadj.enet)
+
+## Save fit statistics
+fit.stats[nrow(fit.stats) + 1, ] = NA
+fit.stats[nrow(fit.stats), c("method", "model")] = c("Elastic Net", "Cytokines")
+fit.stats[nrow(fit.stats), c("rmse", "r2")] = model.unadj.enet$results %>%
+      filter(RMSE == min(RMSE)) %>% 
+      select(RMSE, Rsquared)
+
+## Save LOOCF predictions
+tuningvalues = as.numeric(model.unadj.enet$finalModel$tuneValue)
+dat$pred.unadj.enet = model.unadj.enet$pred %>% 
+      filter(alpha == tuningvalues[1] & lambda == tuningvalues[2]) %>% 
+      arrange(rowIndex) %>% pull(pred)
+
+
+# 3.1.4 Combined-----------------------------------------
+
+
+set.seed(1)
+model.adj.enet = train(x = dat[,c(x, covariates)], y = dat[,y], 
+                         method = "glmnet", metric = "RMSE", #alpha = 0.5,
+                         trControl = fitControl)
+print(model.adj.enet)
+
+## Save fit statistics
+fit.stats[nrow(fit.stats) + 1, ] = NA
+fit.stats[nrow(fit.stats), c("method", "model")] = c("Elastic Net", "Baseline+Cytokines")
+fit.stats[nrow(fit.stats), c("rmse", "r2")] = model.adj.enet$results %>%
+      filter(RMSE == min(RMSE)) %>% 
+      select(RMSE, Rsquared)
+
+
+## Save LOOCF predictions
+tuningvalues = as.numeric(model.adj.enet$finalModel$tuneValue)
+dat$pred.adj.enet = model.adj.enet$pred %>% 
+      filter(alpha == tuningvalues[1] & lambda == tuningvalues[2]) %>% 
+      arrange(rowIndex) %>% pull(pred)
+
 
 
 # 3.2 Random Forest--------------------------------------
@@ -64,10 +176,93 @@ train(x = dat[,-y], y = dat[,y],
 # Surpassing Variable importance threshold?
 
 
+# 3.2.1 Null Model---------------------------------------
+
+set.seed(1)
+model.null.rf = train(x = dat[,c(x, covariates)], y = sample(dat[,y], replace = FALSE), 
+                      method = "rf", metric = "RMSE", #alpha = 0.5,
+                      trControl = fitControl)
+print(model.null.rf)
+
+## Save fit statistics
+fit.stats[nrow(fit.stats) + 1, ] = NA
+fit.stats[nrow(fit.stats), c("method", "model")] = c("Random Forest", "Null")
+fit.stats[nrow(fit.stats), c("rmse", "r2")] = model.null.rf$results %>%
+      filter(RMSE == min(RMSE)) %>% 
+      select(RMSE, Rsquared)
 
 
 
-# 4 Content validity analysis-----------------------------
+# 3.2.2 Baseline Model-----------------------------------
+
+set.seed(1)
+model.base.rf = train(x = dat[,covariates], y = dat[,y], 
+                        method = "rf", metric = "RMSE", #alpha = 0.5,
+                        trControl = fitControl)
+print(model.base.rf)
+
+## Save fit statistics
+fit.stats[nrow(fit.stats) + 1, ] = NA
+fit.stats[nrow(fit.stats), c("method", "model")] = c("Random Forest", "Baseline")
+fit.stats[nrow(fit.stats), c("rmse", "r2")] = model.base.rf$results %>%
+      filter(RMSE == min(RMSE)) %>% 
+      select(RMSE, Rsquared)
+
+
+## Save LOOCF predictions
+dat$pred.base.rf = model.base.rf$pred %>% 
+      filter(mtry == as.numeric(model.base.rf$bestTune)) %>% 
+      arrange(rowIndex) %>% pull(pred)
+
+
+# 3.1.3 Cytokine Model-----------------------------------
+
+
+set.seed(1)
+model.unadj.rf = train(x = dat[,x], y = dat[,y], 
+                         method = "rf", metric = "RMSE", #alpha = 0.5,
+                         trControl = fitControl)
+print(model.unadj.rf)
+
+## Save fit statistics
+fit.stats[nrow(fit.stats) + 1, ] = NA
+fit.stats[nrow(fit.stats), c("method", "model")] = c("Random Forest", "Cytokines")
+fit.stats[nrow(fit.stats), c("rmse", "r2")] = model.unadj.rf$results %>%
+      filter(RMSE == min(RMSE)) %>% 
+      select(RMSE, Rsquared)
+
+
+## Save LOOCF predictions
+dat$pred.unadj.rf = model.unadj.rf$pred %>% 
+      filter(mtry == as.numeric(model.unadj.rf$bestTune)) %>% 
+      arrange(rowIndex) %>% pull(pred)
+
+
+# 3.1.4 Combined-----------------------------------------
+
+
+set.seed(1)
+model.adj.rf = train(x = dat[,c(x, covariates)], y = dat[,y], 
+                     method = "rf", metric = "RMSE", #alpha = 0.5,
+                     trControl = fitControl)
+print(model.adj.rf)
+
+## Save fit statistics
+fit.stats[nrow(fit.stats) + 1, ] = NA
+fit.stats[nrow(fit.stats), c("method", "model")] = c("Random Forest", "Baseline+Cytokines")
+fit.stats[nrow(fit.stats), c("rmse", "r2")] = model.adj.rf$results %>%
+      filter(RMSE == min(RMSE)) %>% 
+      select(RMSE, Rsquared)
+
+
+## Save LOOCF predictions
+dat$pred.adj.rf = model.adj.rf$pred %>% 
+      filter(mtry == as.numeric(model.adj.rf$bestTune)) %>% 
+      arrange(rowIndex) %>% pull(pred)
+
+
+
+# 4 Content validity analysis----------------------------
 
 
 # 4.1 MADRS----------------------------------------------
@@ -86,4 +281,119 @@ train(x = dat[,-y], y = dat[,y],
 # 4.5 Dropout--------------------------------------------
 
 
+# 5 Visualisation----------------------------------------
+
+
+# 5.1 Preparation----------------------------------------
+
+## Create data
+vis_dat = with(dat, 
+               data.frame(t7_bdi_locf = rep(t7_bdi_locf, 6),
+                          t7_bdi_pred = c(pred.base.enet, pred.unadj.enet, pred.adj.enet,
+                                          pred.base.rf, pred.unadj.rf, pred.adj.rf),
+                          method = c(rep("Elastic Net", 3*nrow(dat)), 
+                                     rep("Random Forest", 3*nrow(dat))),
+                          model = rep(rep(c("Baseline", "Cytokines", "Baseline+Cytokines"), 
+                                          each = nrow(dat)), 2),
+                          r2 = rep(c(summary(lm(t7_bdi_locf ~ pred.base.enet))$r.squared, 
+                                     summary(lm(t7_bdi_locf ~ pred.unadj.enet))$r.squared, 
+                                     summary(lm(t7_bdi_locf ~ pred.adj.enet))$r.squared,
+                                     summary(lm(t7_bdi_locf ~ pred.base.rf))$r.squared, 
+                                     summary(lm(t7_bdi_locf ~ pred.unadj.rf))$r.squared, 
+                                     summary(lm(t7_bdi_locf ~ pred.adj.rf))$r.squared),
+                                   each = nrow(dat))
+               )
+)
+
+## Set factor leves
+vis_dat$model = factor(vis_dat$model, levels = c("Baseline", "Cytokines", "Baseline+Cytokines"))
+
+
+
+
+
+# 5.2 Cytokine Correlations------------------------------
+
+
+## Save correlation matrix and p-value matrix
+cyto_cor = cor(dat[, cyto_ref$vars], use = "pairwise.complete.obs", method = "pearson")
+cyto_cor_pmat = cor_pmat(dat[, cyto_ref$vars])
+
+## Set labels 
+colnames(cyto_cor) = cyto_ref$labels
+rownames(cyto_cor) = cyto_ref$labels
+colnames(cyto_cor_pmat) = cyto_ref$labels
+rownames(cyto_cor_pmat) = cyto_ref$labels
+
+# Reduce to those cytokines with no missing values (i.e., used in analyses)
+cyto_cor = cyto_cor[row.names(cyto_cor) %in% cyto_ref[cyto_ref$no.na == 1, "labels"],
+                    colnames(cyto_cor) %in% cyto_ref[cyto_ref$no.na == 1, "labels"]]
+cyto_cor_pmat = cyto_cor_pmat[row.names(cyto_cor_pmat) %in% cyto_ref[cyto_ref$no.na == 1, "labels"],
+                              row.names(cyto_cor_pmat) %in% cyto_ref[cyto_ref$no.na == 1, "labels"]]
+
+## Export with width=; height=
+ggcorrplot(cyto_cor, 
+           hc.order = TRUE, type = "lower", 
+           legend.title = "", show.legend = TRUE,
+           #p.mat = cyto_cor_pmat,
+           outline.col = "white", lab = TRUE, digits = 1, lab_size = 3,
+           #ggtheme = ggplot2::theme_minimal(),
+           colors = c(brewer.pal(3, "Dark2")[1], "white", brewer.pal(3, "Dark2")[2])) 
+
+
+
+# 5.3 ML Prediction Scatter Plots------------------------
+
+## Prediction (export with: width = 800, height = standard)
+ggplot(vis_dat, aes(x = t7_bdi_pred, y = t7_bdi_locf)) +
+      geom_smooth(aes(col = model, fill = model), method = "lm") +
+      geom_point(aes(fill = model), col = "black", shape = 21, size = 2.5) +
+      facet_grid(model~method) +
+      #geom_text(x = 10, y = 40, label = expression("Spearman's"~rho == 0.34), parse = TRUE) +
+      labs(x = "BDI: Predicted Symptom Score", y = "BDI: Actual Symptom Score",
+           fill = "Model: ", col = "Model: ") +
+      scale_fill_brewer(palette = "Dark2") +
+      scale_color_brewer(palette = "Dark2") +
+      theme_bw() +
+      theme(legend.position = "top",
+            #strip.text.x = element_text(colour = "black"),
+            #strip.background = element_rect(colour = "black", fill = "white")
+            )
+
+
+
+# 5.3 Absolute Residual Boxplots-------------------------
+
+## Calculate absolute residuals
+vis_dat$abs_residuals = with(vis_dat, abs(t7_bdi_locf - t7_bdi_pred))
+
+## Plot
+ggplot(vis_dat, aes(x = method, y = abs_residuals)) +
+      geom_boxplot(aes(fill = model), alpha = 0.6, position = position_dodge(width = 0.8),
+                   outlier.alpha = 0) +
+      geom_jitter(aes(fill = model), shape = 21, col = "black", 
+                  position = position_jitterdodge(dodge.width = 0.8)) +
+      scale_fill_brewer(palette = "Dark2") +
+      labs(x = "Model", y = "Absolute Residuals") +
+      theme_bw() +
+      theme(legend.position = "top",
+            legend.title = element_blank())
+
+
+
+# 5.5 Fit Statistics-------------------------------------
+
+## Set factor levels
+fit.stats$model = factor(fit.stats$model, levels = c("Null", "Baseline", "Cytokines", 
+                                                     "Baseline+Cytokines"))
+
+ggplot(fit.stats, aes(x = method, y = r2)) +
+      geom_bar(aes(fill = model), stat = "identity", position = position_dodge(), col = "black") +
+      scale_fill_brewer(palette = "Dark2") +
+      scale_y_continuous(limits = c(0, 0.15)) +
+      coord_cartesian(expand = FALSE) +
+      labs(x = "Model", y = expression(R^2)) +
+      theme_bw() +
+      theme(legend.position = "top",
+            legend.title = element_blank())
 
--- Scripts/Preparation.R
+++ Scripts/Preparation.R
@@ -9,26 +9,45 @@
 
 ## Load Packages
 library("tidyverse")
+library("readxl")
 
 ## Load psychometric data
 load("/home/nkappelmann/OPTIMA/OPTIMA_Analyses/Data/Processed/02_PsychometricData.RData")
 
-## Load cytokine data
+## Load cytokine data and related IDs
 cyto_dat = readRDS("/binder/mgp/datasets/2020_ImmuneDepression/cytokine/01_data_for_analysis.rds")
+cyto_IDs = read_excel("/binder/mgp/datasets/2020_ImmuneDepression/cytokine/01_become_optima_randomization_STD_Controls_Pheno_1219.xlsx")
 
-# Save Study IDs
-cyto_dat$ID = row.names(cyto_dat)
+# Save biobank study IDs
+cyto_dat$ID.bio = row.names(cyto_dat)
+
+# Merge OPTIMA IDs
+cyto_dat = merge(cyto_dat, cyto_IDs[, c("ID", "Lagerung_BC.Tube")], 
+                 by.x = "ID.bio", by.y = "Lagerung_BC.Tube", all.x = TRUE)
+
+# Delete duplicates from merging
+cyto_dat = cyto_dat[!duplicated(cyto_dat$ID),]
 
 # Save variable names in cytokine reference
 cyto_ref = data.frame(vars = NA,
-                      labels = colnames(cyto_dat)[8:ncol(cyto_dat)])
+                      labels = colnames(cyto_dat)[9:ncol(cyto_dat)])
 
 # Change column names by excluding hyphens and slashes
 colnames(cyto_dat) = gsub("/", "_", colnames(cyto_dat), fixed = TRUE)
 colnames(cyto_dat) = gsub("-", ".", colnames(cyto_dat), fixed = TRUE)
 colnames(cyto_dat) = gsub(" ", "_", colnames(cyto_dat), fixed = TRUE)
-cyto_ref$vars = colnames(cyto_dat)[8:ncol(cyto_dat)]
+cyto_ref$vars = colnames(cyto_dat)[9:ncol(cyto_dat)]
+cyto_ref = cyto_ref[cyto_ref$vars != "ID",]
 
+## Remove BeCOME data
+cyto_dat = cyto_dat[grepl("PTP", cyto_dat$ID, fixed = TRUE),]
+
+## Remove OPTIMA participants without cytokine data
+# Note: PTP0114 is excluded as this person clicked through all of their outcome data.
+dat = dat[dat$ID %in% cyto_dat$ID,]
+
+## Remove age and sex, so these don't get duplicated
+dat[, c("sex", "age")] = NULL
 
 ## Merge psychometric and cytokine data
 dat = merge(dat, cyto_dat, by = "ID", all.x = TRUE)
@@ -36,15 +55,19 @@ dat = merge(dat, cyto_dat, by = "ID", all.x = TRUE)
 
 # 1.2 Coding---------------------------------------------
 
+## Create binarised and standardised sex variable
+dat$sex_std = ifelse(dat$sex == "female", 0.5, -0.5)
 
-## CRP cut-off >3mg/L
-dat$CRP_greater3 = factor(ifelse(dat$hsCRP > 3, ">3mg/L", "<=3mg/L"),
-                          levels = c("<=3mg/L", ">3mg/L"))
+## Create standardised age variable
+dat$age_std = scale(dat$age)
 
+## Create standardised t0_bdi variable
+dat$t0_bdi_std = scale(dat$t0_bdi)
 
-## Sex as binary number and category
-dat$sex_num = ifelse(dat$sex2 == "weiblich", 1, 0)
-dat$sex_cat = ifelse(dat$sex2 == "weiblich", "Female", "Male")
+## Get Top 27% CRP cut-off
+dat$CRP_inflamed = factor(ifelse(dat$CRP > quantile(dat$CRP, prob=1-27/100), 
+                                 "Inflamed", "Non-Inflamed"),
+                          levels = c("Non-Inflamed", "Inflamed"))
 
 
 ## Ward
@@ -63,6 +86,13 @@ dat$employed = ifelse(dat$employed == 1, "Employed", "Unemployed")
 dat[!is.na(dat$unemploymentreason) & dat$unemploymentreason == "retired", "employed"] = "Retired" 
 
 
+## Set binary variable indicating if cytokine data are complete (following imputation)
+cyto_ref[, "na.sum"] = NA
+for(i in cyto_ref$vars) {
+      cyto_ref[cyto_ref$vars == i, "na.sum"] = sum(is.na(dat[, i]))
+}
+cyto_ref$no.na = ifelse(cyto_ref$na.sum >= 1, 0, 1)
+
 
 # 2 Preprocessing----------------------------------------
 
@@ -75,12 +105,15 @@ dat$t7_bdi_locf = NA
 
 ## Run loop to carry forward the last observed bdi-value
 for(i in 1:nrow(dat))   {
-      for(j in 7:1)   {
+      for(j in 7:0)   {
             if(is.na(dat[i, "t7_bdi_locf"]) & !is.na(dat[i, paste0("t", j, "_bdi")]))     {
                   dat[i, "t7_bdi_locf"] = dat[i, paste0("t", j, "_bdi")]
                   }
       }
 }
+
+## Create change variable by subtracting the t0 from the t7_locf score.
+dat$bdi_locf_improve = dat$t0_bdi - dat$t7_bdi_locf
 
 
 # 2.1.2 MADRS--------------------------------------------
@@ -117,7 +150,7 @@ for(i in 1:nrow(dat))   {
 # 5 Save Data--------------------------------------------
 
 ## Save main data
-save(dat, file = "")
+save(dat, file = "/binder/mgp/datasets/2020_ImmuneDepression/cytokine/Nils_Preprocessed/OPTIMA_Cytokine_PreprocessedData.RData")
 
 ## Save info of inflammatory variables
-save(cyto_ref, file = "")
+save(cyto_ref, file = "/binder/mgp/datasets/2020_ImmuneDepression/cytokine/Nils_Preprocessed/Cytokine_Reference.RData")
